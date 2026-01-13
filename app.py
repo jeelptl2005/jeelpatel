58b6805f520672f59121673944b553084b2de04b
JEEL PATEL <jeelptl2005@gmail.com>
2026-01-13 10:43:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,15 +21,15 @@ def submit():
         body1= f"""Subject: {subject1}\n\nYou received a new response from you Portfolio.\n\nName: {name}\nEmail: {email}\nMessage: {message}"""
         body2= f"""Subject: {subject2}\n\nDear {name},\n\nThank you for reaching out through my portfolio website!\n\nI have received your message and will get back as soon as possible.\n\nBest regards,\nJEEL PATEL.\nFull-Stack Developer."""
 
-        FROM= "jeelptl2005@gmail.com"
-        PASSWORD= "xnnp tdpt gdqu rutn"
+        FROM= os.getenv("GMAIL")
+        PASSWORD= os.getenv("GMAIL_PASS")
 
         try:
             with s.SMTP("smtp.gmail.com",587) as server:
                 server.starttls()
                 server.login(FROM,PASSWORD)
                 server.sendmail(FROM,email,body2)
-                server.sendmail(email, FROM, body1)
+                server.sendmail(FROM, FROM, body1)
             flash("Message sent successfully","success")
             return redirect(url_for("home"))
         except:
@@ -43,6 +43,7 @@ def sitemap():
 
 port = int(os.environ.get("PORT", 8080))
 app.run(debug=True,host="0.0.0.0", port=port)
+
 
 
 
